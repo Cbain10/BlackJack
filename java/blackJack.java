c72da947519ef9b59381aede3e546ed99498a5de
Christian Bain <50216002+Cbain10@users.noreply.github.com>
2020-10-07 11:34:56 -0600

    finished myCards
    dealing userCards is done. Need to add dealer getting new cards and end game result

--- java/blackJack.java
+++ java/blackJack.java
@@ -1,79 +1,137 @@
+package com.company;
+import javax.print.DocFlavor;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.*;
 
 class Main {
 
-  public static void main(String[] args) {
+    public static int upperBound = 0;
 
-    Vector deck = new Vector();
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 11; i++) {
-      for (int j = 0; j < 4; j++) {
-        deck.add(i);
-        if (i == 10) {
-          deck.add(i);
-          deck.add(i);
+        Vector<Integer> deck = new Vector<Integer>();
+
+        for (int i = 1; i <= 11; i++) {
+            for (int j = 0; j < 4; j++) {
+                deck.add(i);
+                if (i == 10) {
+                    deck.add(i);
+                    deck.add(i);
+                }
+            }
         }
-      }
+
+        System.out.println("Welcome to BLACKJACK!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you ready to play? y/n ");
+        String userInput = scanner.nextLine();
+
+        Random rand = new Random();
+        int card;
+        int dealerCard;
+        int total;
+
+        while (userInput.equals("y")) {
+            Vector<Integer> myCards = new Vector<Integer>();
+            Vector<Integer> dealerHand = new Vector<Integer>();
+            upperBound = deck.size();
+            total = 0;
+
+            card = dealNewCard(deck, rand, upperBound);
+            myCards.add(card);
+            total += card;
+            upperBound--;
+            dealerCard = dealNewCard(deck, rand, upperBound);
+            dealerHand.add(dealerCard);
+            upperBound--;
+            card = dealNewCard(deck, rand, upperBound);
+            myCards.add(card);
+            total += card;
+            upperBound--;
+            if (myCards.get(0) == 11 && myCards.get(1) == 11) {
+                total = 12;
+                myCards.set(0,1);
+            }
+            if (myCards.contains(11) && total > 21) {
+                total = checkAce(myCards);
+            }
+            printResults(myCards, total, dealerCard);
+
+            if (total < 21) {
+                System.out.println("Hit? y/n ");
+                userInput = scanner.nextLine();
+            } else if (total == 21) {
+                System.out.println("Your total is 21!");
+                userInput = "n";
+            }
+            while (userInput.equals("y") && (total < 21)) {
+                card = dealNewCard(deck, rand, upperBound);
+                myCards.add(card);
+                upperBound--;
+                total += card;
+                if (myCards.contains(11) && total > 21) {
+                    total = checkAce(myCards);
+                }
+                printResults(myCards, total, dealerCard);
+                if (total > 21) {
+                    if (myCards.contains(11)) {
+                        boolean aceFound = false;
+                        int aceTotal = 0;
+                        for (int i = 0; i < myCards.size(); i++) {
+                            aceTotal += myCards.get(i);
+                            if ((myCards.get(i) == 11) && (!aceFound)) {
+                                myCards.set(i, 1);
+                                aceFound = true;
+                            }
+                        }
+                        total = aceTotal;
+                    } else {
+                        System.out.println("YOU BROKE!");
+                        userInput = "n";
+                    }
+                } else if (total == 21) {
+                    userInput = "n";
+                } else {
+                    System.out.println("Hit? y/n ");
+                    userInput = scanner.nextLine();
+                }
+            }
+            // check results
+
+
+            System.out.println("Play again? y/n ");
+            userInput = scanner.nextLine();
+        }
     }
 
-    System.out.println("Welcome to BLACKJACK!");
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Are you ready to play? y/n ");
-    String userInput = scanner.nextLine();
-
-    Random rand = new Random();
-    int card1_index = 0;
-    int card2_index = 0;
-    int card1 = 0;
-    int card2 = 0;
-    int dealerCard = 0;
-    
-    while (userInput.equals("y")) {
-      System.out.println("ENTER WHILE LOOP");
-      Vector myCards = new Vector();
-      int upperBound = deck.size();
-      card1_index = rand.nextInt(upperBound);
-
-      // card1 = deck.get(card1_index);
-      deck.remove(card1_index);
-      upperBound--;
-      card2_index = rand.nextInt(upperBound);
-      // card2 = deck.get(card2_index);
-      deck.remove(card2_index);
-      upperBound--;
-      System.out.println("Dealer ")
-
-
-
-      userInput = scanner.nextLine();
-
-
-
-
-
+    public static int checkAce(Vector<Integer> myCards) {
+        int aceTotal = 0;
+        boolean aceFound = false;
+        for (int i = 0; i < myCards.size(); i++) {
+            if (myCards.get(i) == 11 && !aceFound) {
+                myCards.set(i, 1);
+                aceFound = true;
+            }
+            aceTotal += myCards.get(i);
+        }
+        return aceTotal;
     }
 
+    public static void printResults(Vector<Integer> myCards, int total, int dealerCard) {
+        System.out.println("Dealer's hand: " + dealerCard);
+        System.out.print("Your hand: ");
+        for (Integer myCard : myCards) {
+            System.out.print(myCard + " ");
+        }
+        System.out.println();
+        System.out.println("Total: " + total);
+    }
 
-    // 17 or higher stay
-    // 16 or lower must hit
-    // Scanner scanner = new Scanner(System.in);
-    // String playAgain = "y";
-    // while (playAgain == 'y') {
-    //   // receive two cards
-    //   Random rand = new Random();
-    //   int upperBound = cards.size();
-      
-
-
-
-      
-
-
-
-
-
-
-  }
+    public static int dealNewCard(Vector<Integer> deck, Random rand, int upperBound) {
+        int randomIndex = rand.nextInt(upperBound);
+        int card = deck.get(randomIndex);
+        deck.remove(randomIndex);
+        return card;
+    }
 }
